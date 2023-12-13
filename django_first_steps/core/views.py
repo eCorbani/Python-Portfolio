95b89ae5b5912db00c56516394c7985b4b7ccf0d
Edson Luis Corbani <54867187+eCorbani@users.noreply.github.com>
2023-12-13 18:26:01 +0000

    Delete django_first_steps directory

--- django_first_steps/core/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import (render,
-                              HttpResponse)
-
-# Create your views here.
-
-def hello(request, name):
-    return HttpResponse('<h1>Hello {}</h1>'.format(name))
